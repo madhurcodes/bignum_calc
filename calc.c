17a64101b45d750e07ceb05a56c252a858aa3d2c
madhurcodes <cs1150235@iitd.ac.in>
2017-02-12 11:17:38 +0530

    Some changes

--- calc.c
+++ calc.c
@@ -23,9 +23,15 @@ bignum* logarithm(bignum  *inp);
 bignum*  power(bignum *a, bignum *b);
 char* bignum_to_string(bignum *inp);
 bignum* string_to_bignum(char *inp);
-bignum* subtract_bignum(bignum* a , bignum* b);
-bignum* add_bignum(bignum* a , bignum* b);
-int is_greater(bignum* a , bignum* b);
+void print(bignum *a);
+void add_bignum_positive(bignum *a, bignum *b, bignum *c);
+bignum* add_bignum(bignum *a, bignum *b);
+bignum* complement(bignum *a);
+bignum* subtract_bignum(bignum *a, bignum *b);
+void align_decimals(bignum *a, int shift_amount);
+void remove_leading_zeroes(bignum *c);
+int is_greater(bignum *a, bignum*b);
+void subtract_bignum_positive(bignum* a, bignum* b,bignum* c);
 
 bignum* product_with_digit(bignum *inp, int dig){
 	int carry=0;
@@ -59,6 +65,12 @@ bignum* product(bignum *a, bignum *b){
 	}
 	ret->decimal_point = a->decimal_point + b->decimal_point;
 	ret->last_digit = find_last_index(ret);
+	if(a->sign==b->sign){
+		ret->sign = 1;
+	}
+	else{
+		ret->sign  = -1;
+	}
 	//round  
 	return ret;
 }
@@ -90,13 +102,26 @@ int divide_guess_digit(bignum* a, bignum* b){
 }
 bignum* divide(bignum *a, bignum *b){
 	// ignore decimals in inputs initially, taking care of them later 
-	// use isgreater for compare 1 if a>b -1 otherwise
+	// use is_greater for compare 1 if a>b -1 otherwise
 	// I store dec points of inputs at start and cange them, will change back at end,
 	int deca, decb, signa,signb;
 	deca = a->decimal_point;
 	signa = a->sign;
 	signb = b->sign;
 	decb = b->decimal_point;
+	int decimalpoints = deca - decb;
+	int kkk;
+	if(decimalpoints>=0){
+		
+	}
+	else{
+		for(kkk= -decimalpoints;kkk>0 ;kkk--){
+			a = product(a,bigify_int(10));
+		}
+	}
+	
+	
+	
 	a->decimal_point = 0 ;
 	b->decimal_point = 0 ;
 	a->sign = 1 ;
@@ -140,6 +165,16 @@ bignum* divide(bignum *a, bignum *b){
 	b->decimal_point = decb ;
 	a->sign = signa ;
 	b->sign = signb ;
+	if(decimalpoints>=0){
+		ret->decimal_point += decimalpoints;
+		remove_leading_zeroes(ret);
+	}
+	if(a->sign==b->sign){
+		ret->sign = 1;
+	}
+	else{
+		ret->sign  = -1;
+	}
 	return ret;
 }
 bignum* divide_single_digit(bignum* a, int b){
@@ -318,8 +353,8 @@ char* bignum_to_string(bignum *inp){
 
 
 
-//////temp
-bignum* add_bignum(bignum* a , bignum* b){
+//////////////////////////////////////////////////////////////////
+/* bignum* add_bignum(bignum* a , bignum* b){
 	char * tee;
 	sprintf(tee, "%f", atof(bignum_to_string(a)) + atof(bignum_to_string(b)));
 	printf("--%s--",tee);
@@ -338,9 +373,282 @@ int is_greater(bignum* a , bignum* b){
 	else{
 		return -1;
 	}
+} */
+
+
+void print(bignum *a){
+	if(a->sign>0)
+		printf("+");
+	else
+		printf("-");
+	for(int i=a->last_digit; i>=0; i--){
+		printf("%d",a->digits[i]);
+	}
+	printf("\n");
+	//printf("f=%d  sgn=%d d=%d\n", a->last_digit, a->sign, a->decimal_point);
 }
 
-//////
+void print_rep(bignum *a){ //prints array representation
+	for(int i=0; i<=a->last_digit; i++){
+		printf("%d",a->digits[i]);
+	}
+	printf("\n");
+}
+
+//not worried about rounding off since printing will take care of them
+
+void add_bignum_positive(bignum *a, bignum *b, bignum *c) {
+	int carry = 0;
+	int dec1 = a->decimal_point;
+	int dec2 = b->decimal_point;
+	int e = (dec2<dec1) ? dec2 : dec1;
+	int d = (dec2<dec1) ? dec1 : dec2;
+	int flag;
+	//shift and align decimals
+	if(dec1<dec2) {
+		flag=1;
+		align_decimals(a, d-e);
+		c->decimal_point = b->decimal_point;
+	}
+	else{
+		flag=2;
+		align_decimals(b, d-e);
+		c->decimal_point = a->decimal_point;
+	}
+	int i;
+	for(i=0; i <= ((a->last_digit > b->last_digit) ? (a->last_digit) : (b->last_digit)) ; i++) {
+		c->digits[i] = (a->digits[i] + b->digits[i] + carry)%10;
+		//printf("c, i  = %d,%d\n", c->digits[i],i);
+		carry = (a->digits[i] + b->digits[i] + carry)/10;
+	}
+	c->digits[i] = carry;
+	if(c->digits[i]!=0)
+		c->last_digit=i;
+	else
+		c->last_digit=i-1;
+	remove_leading_zeroes(c);
+	//reset a and b to normal
+	if(flag ==1)
+		remove_leading_zeroes(a);
+	else
+		remove_leading_zeroes(b);
+}
+
+bignum* add_bignum(bignum *a, bignum *b) {
+	bignum*c = (bignum *)malloc(sizeof(bignum)); //does c = a+b
+	if(a->sign >0 && b->sign>0){
+		c->sign = 1;
+		add_bignum_positive(a,b,c);
+	}
+	if(a->sign <0 && b->sign<0){
+		c->sign = -1;
+		add_bignum_positive(a,b,c);
+	}
+	if(a->sign >0 && b->sign<0){
+		b->sign = 1;
+		int temp = is_greater(a,b);
+		c->sign = temp;
+		if(temp ==1 ){
+			subtract_bignum_positive(a,b,c);
+		}
+		else{
+			subtract_bignum_positive(b,a,c);
+		}
+		 //doesn't matter that we haven't initialized sign of complement(b), since it isn't used anywhere
+		b->sign = -1;
+	}
+
+	if(a->sign <0 && b->sign>0){
+		a->sign = 1;
+		int temp = is_greater(a,b);
+		c->sign = -temp;
+		if(temp ==1 )
+			subtract_bignum_positive(a,b,c);
+		else
+			subtract_bignum_positive(b,a,c);
+		a->sign = -1;
+	}
+	remove_leading_zeroes(c);
+	return c;
+}
+
+bignum* complement(bignum *a){
+	}
+
+void subtract_bignum_positive(bignum* a, bignum* b,bignum* c){// c = a-b a>b
+	int carry = 0;
+	int dec1 = a->decimal_point;
+	int dec2 = b->decimal_point;
+	int e = (dec2<dec1) ? dec2 : dec1;
+	int d = (dec2<dec1) ? dec1 : dec2;
+	int flag;
+	//shift and align decimals
+	if(dec1<dec2) {
+		flag=1;
+		align_decimals(a, d-e);
+		c->decimal_point = b->decimal_point;
+	}
+	else if (dec1>dec2){
+		flag=2;
+		align_decimals(b, d-e);
+		c->decimal_point = a->decimal_point;
+	}
+	int limit = ((a->last_digit > b->last_digit) ? a->last_digit : b->last_digit);
+	for(int i=0; i<= limit; i++) {
+		int temp = a->digits[i] - b->digits[i] + carry;
+		if (temp<0) {
+			c->digits[i] = 10 + temp ;
+			carry = -1;
+		}
+		else{
+			c->digits[i] = temp;
+			carry = 0;
+		}
+
+	}
+	c->last_digit = limit;
+
+}
+
+
+/*void make_absolute(bignum *a) {
+	bignum* c = (bignum *) malloc(sizeof(bignum));
+	if(a->sign>0)
+		c->sign=1;
+	else
+		c->sign =-1;
+	c->decimal_point = a->decimal_point;
+}*/
+
+/*void negate(bignum *a) {
+	a->sign = -a->sign;
+}*/
+
+int is_greater(bignum *a, bignum*b){ //checks if a>b if yes then 1 else -1 
+	if(a->sign>0 && b->sign<0)
+		return 1;
+	if(a->sign<0 && b->sign>0)
+		return -1;
+	if(a->sign>0 && b->sign>0){
+		if(a->last_digit - a->decimal_point > b->last_digit - b->decimal_point)
+			return 1;
+		else if (a->last_digit - a->decimal_point < b->last_digit - b->decimal_point)
+			return -1;
+		else {
+			int dec1 = a->decimal_point;
+			int dec2 = b->decimal_point;
+			int c = (dec2<dec1) ? dec2 : dec1;
+			int d = (dec2<dec1) ? dec1 : dec2;
+			int flag;
+			if(dec1<dec2) 
+				align_decimals(a, d-c);
+			else
+				align_decimals(b, d-c);
+			for(int i= a->last_digit; i>=0; i--){
+				if(a->digits[i]>b->digits[i])
+					return 1;
+				if(a->digits[i]<b->digits[i])
+					return -1;
+			}
+			remove_leading_zeroes(a);
+			remove_leading_zeroes(b);
+			return -1;
+			
+		}
+	}
+
+	if(a->sign<0 && b->sign<0){
+		if(a->last_digit - a->decimal_point > b->last_digit - b->decimal_point)
+			return -1;
+		else if (a->last_digit - a->decimal_point < b->last_digit - b->decimal_point)
+			return 1;
+		else {
+			int dec1 = a->decimal_point;
+			int dec2 = b->decimal_point;
+			int c = (dec2<dec1) ? dec2 : dec1;
+			int d = (dec2<dec1) ? dec1 : dec2;
+			int flag;
+			if(dec1<dec2) 
+				align_decimals(a, d-c);
+			else
+				align_decimals(b, d-c);
+			for(int i= a->last_digit; i>=0; i--){
+				if(a->digits[i]>b->digits[i])
+					return -1;
+				if(a->digits[i]<b->digits[i])
+					return 1;
+			}
+			remove_leading_zeroes(a);
+			remove_leading_zeroes(b);
+			return 1;
+		}
+	}
+}
+
+bignum* subtract_bignum(bignum *a, bignum *b) {
+	bignum *c = (bignum *)malloc(sizeof(bignum)); //Does c=a-b
+	if(a->sign >0 && b->sign<0){
+		c->sign = 1;
+		add_bignum_positive(a,b,c);
+	}
+	if(a->sign <0 && b->sign>0){
+		c->sign = -1;
+		add_bignum_positive(a,b,c);
+	}
+	if(a->sign >0 && b->sign>0){
+		int temp = is_greater(a,b);
+		c->sign = temp;
+		if(temp==1)
+			subtract_bignum_positive(a,b,c);
+		else
+			subtract_bignum_positive(b,a,c);
+		
+	}
+
+	if(a->sign <0 && b->sign<0){
+		a->sign = 1;
+		b->sign = 1;
+		int temp = is_greater(a,b);
+		c->sign = -temp;
+		
+		if(temp==1)
+			subtract_bignum_positive(a,b,c);
+		else
+			subtract_bignum_positive(b,a,c);
+		a->sign = -1;
+		b->sign = -1;
+	}
+	return c;
+}
+
+
+
+void align_decimals(bignum *a, int shift_amount) {
+	int temp = a->last_digit;
+	for(int i=0; i<shift_amount; i++){
+		a->digits[temp+shift_amount] = a->digits[temp];
+		temp--;
+	}
+	for(int i=0; i<shift_amount; i++)
+		a->digits[i]=0;
+	a->last_digit += shift_amount;
+	a->decimal_point += shift_amount;
+
+}
+
+void remove_leading_zeroes(bignum *c) {
+	int a=0;
+	while(c->digits[a]==0 && a<c->decimal_point) {
+		a++;
+	}
+	for(int i=0;i<=c->last_digit;i++){
+		c->digits[i]=c->digits[i+a];
+	}
+	c->last_digit -=a ;
+	c->decimal_point -= a;
+}
+
+/////////////////////////////////////////////////////
 
 
 
@@ -358,17 +666,27 @@ int main(){
 	bignum *v=  malloc(sizeof *v);
 
 	v->sign =1;
-	v->digits[0] = 9;
-	v->digits[1] = 6;
-	v->digits[2] = 9;
-	v->digits[3] = 8;
-	v->decimal_point = 2;
-	v->last_digit= 3;
+	v->digits[0] = 1;
+	v->digits[1] = 1;
+	//v->digits[2] = 1;
+	//v->digits[3] = 8;
+	v->decimal_point = 1;
+	v->last_digit= 1;
+	bignum *t=  malloc(sizeof *t);
+
+	t->sign =1;
+	t->digits[0] = 0;
+	//t->digits[1] = 1;
+	//v->digits[2] = 1;
+	//v->digits[3] = 8;
+	t->decimal_point = 0;
+	t->last_digit= 0;
 	print_big(v);
 	printf("\n");
 	print_big(product_with_digit(v,8));
-	printf("%s",bignum_to_string(string_to_bignum(bignum_to_string(string_to_bignum(bignum_to_string(v))))));
-	
+	printf("%s\n",bignum_to_string(string_to_bignum(bignum_to_string(string_to_bignum(bignum_to_string(v))))));
+	print_big(add_bignum(v,t));
+
 
 	return 0;
 }
